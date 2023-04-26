8af4ac1354e41dd54bb1d603982200c10ac010be
onebarloop <alexanderbayer@posteo.de>
2023-04-26 12:55:01 +0200

    add wildcard route

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,15 +4,29 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 
 class VinylController
 {
 
     #[Route('/')]
-    public function homepage(): Response {
+    public function homepage(): Response
+    {
+        return new Response('Hello Jetbrains!');
+    }
 
-        return new Response('<h1>Hello Jetbrains!</h1>');
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
 
+        if ($slug) {
+            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
+         } else {
+            $title = 'All Genres';
+        }
+
+
+        return new Response ($title);
     }
 }
