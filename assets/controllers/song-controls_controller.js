cddc76f89fc946b61b866f533e692afd44025f9a
onebarloop <alexanderbayer@posteo.de>
2023-04-27 14:55:28 +0200

    add stimulus functionality

--- assets/controllers/song-controls_controller.js
+++ assets/controllers/song-controls_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from '@hotwired/stimulus';
+import axios from "axios";
 
 /*
  * This is an example Stimulus controller!
@@ -10,7 +11,17 @@ import { Controller } from '@hotwired/stimulus';
  * Delete this file or adapt it for your use!
  */
 export default class extends Controller {
-    connect() {
-        this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
+
+    static values = {
+        infoUrl: String
     }
+
+
+    async play(event) {
+        event.preventDefault();
+        const response = await axios.get(this.infoUrlValue);
+        const audio = new Audio(response.data.url)
+        audio.play();
+    }
+
 }
